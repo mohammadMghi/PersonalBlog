aaa239714a907403b825dad87f0aee45268798bd
mohammad mghi <mohammadmghig@gmail.com>
2020-02-29 23:13:07 +0330

    front-fixed

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,11 @@
 @extends('mainLayouts.main')
 
 @section('content')
-<div class="card">
+<div class="card mb-5">
     <img class="card-img-top" src="/{{$post->cover}}" alt="Card image cap">
     <div class="card-body">
-        <h5 class="card-title">{{$post->title}}</h5>
-        <p class="card-text">{{$post->content}}</p>
+        <h5 class="card-title text-right">{{$post->title}}</h5>
+        <p class="card-text text-right">{{$post->content}}</p>
     </div>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,8 +20,8 @@
         <img class="card-img-top" src="http://localhost:8000/{{$post->cover}}" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title" id="text-limited-post-head">{{$post->title}}</h5>
-            <p class="card-text" id="text-limited-post">{{ str_replace("&nbsp;","",strip_tags($post->content)) }}</p>
-            <p class="card-text"><small class="text-muted">آخرین باری که نوشته شده در {{$post->updated_at}}</small></p>
+            <p class="card-text text-right" id="text-limited-post">{{ str_replace("&nbsp;","",strip_tags($post->content)) }}</p>
+            <p class="card-text text-right"><small class="text-muted">آخرین باری که نوشته شده در {{$post->updated_at}}</small></p>
 
             <a href="http://localhost:8000/post/{{$post->slug}}">ادامه مطلب</a>
         </div>
